42eb2c89a2f1575c49ebdec3858e72285687612c
TylerDonghwi <tylerdonghwikim@gmail.com>
2022-09-03 17:06:28 +1200

    user interface for the job card is implemented

--- src/component/Job.tsx
+++ src/component/Job.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 type jobProps = {
     id: string,
     creationDate: string,
@@ -6,7 +8,40 @@ type jobProps = {
 }
 
 export default function Job({ id, creationDate, client, contact }: jobProps) {
+
+    enum jobStatus {
+        SCHEDULED = 'scheduled',
+        ACTIVE = 'active',
+        INVOICING = 'invoicing',
+        TO_PRICED = 'to priced',
+        COMPLETED = 'completed',
+    }
+
+    const [status, setStatus] = useState<jobStatus>(jobStatus.SCHEDULED)
+
     return (
-        <div>{id} {creationDate} {client} {contact} </div>
+        <div className="m-2 border border-orange-500 shadow-md rounded-md">
+            <div className="m-2 text-lg font-bold">
+                {id}
+            </div>
+            <div className="m-2">
+                <div className="text-gray-700">
+                    created on: {creationDate}
+                </div>
+                <div className="text-sm text-gray-500">
+                    posted by {client} &#xb7; contact: {contact}
+                </div>
+                <div>Status: {status}</div>
+                <button
+                    className='relative text-white rounded-sm p-1 m-1 bg-orange-500 hover:bg-orange-600 active:bg-orange-700'
+                    onClick={() => console.log(`viewing notes for ${id}`)}
+                >Details</button>
+                <button
+                    className='relative text-white rounded-sm p-1 m-1 bg-orange-500 hover:bg-orange-600 active:bg-orange-700'
+                    onClick={() => console.log(`adding note for ${id}`)}
+                >Add Note</button>
+            </div>
+
+        </div >
     )
 }
