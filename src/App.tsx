6190a73bc11d826ca593c8f72801bd40f9b7c445
TylerDonghwi <tylerdonghwikim@gmail.com>
2022-09-03 13:50:42 +1200

    basic data created for jobs in json

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import jobsList from './data/jobs.json'
+
 
 function App() {
+
+  const sortByName = (a: { id: string; }, b: { id: string; }) => (a.id > b.id) ? 1 : ((b.id > a.id) ? -1 : 0)
+  const sortByDate = (a: { creationDate: string; }, b: { creationDate: string; }) => (a.creationDate > b.creationDate) ? 1 : ((b.creationDate > a.creationDate) ? -1 : 0)
+
+
   return (
-    <div className="bg-blue-400">Job Search App</div>
+    <div className="bg-blue-200">
+      {jobsList.sort(sortByDate).map(job => (
+        <div key={job.id}>
+          {job.id}: {job.creationDate}
+        </div>
+      ))}
+    </div>
   );
 }
 
